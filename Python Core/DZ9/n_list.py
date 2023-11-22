a58721cd6e81f8ab74501a2d6165b1c4b79a7376
myataSD <104759126+myataSD@users.noreply.github.com>
2023-11-22 17:03:51 +0300

    n_list.py

--- Python Core/DZ9/n_list.py
+++ Python Core/DZ9/n_list.py
@@ -1,9 +1,18 @@
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            return False
+    return True
 
-n = int(input())
-n_list =[]
+def find_primes(n):
+    primes = []
+    for num in range(1, n + 1):
+        if is_prime(num):
+            primes.append(num)
+    return primes
 
-for i in range(1,n + 1):
-    n_list.append(i)
-    i += 1
-    
-print(n_list)
+n = int(input("Введите число n: "))
+prime_numbers = find_primes(n)
+print("Простые числа от 1 до", n, ":", *prime_numbers)
