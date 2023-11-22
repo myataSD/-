fb9fa2f6a05c89efba2aa72f1c5eebf37d0314a6
myataSD <104759126+myataSD@users.noreply.github.com>
2023-11-22 17:03:22 +0300

    max.py

--- Python Core/DZ8/max.py
+++ Python Core/DZ8/max.py
@@ -1,13 +1,14 @@
-def maximum(list, _max_ = None):
-    if _max_ is None:
-        _max_ = list.pop()
-    current = list.pop()
-    if  current  > _max_:
-        _max_ = current
-    if  list:
-        return maximum(list, _max_)
-    return _max_
+def find_max(num_list):
+    if len(num_list) == 1:
+        return num_list[0]
+    else:
+        max_rest = find_max(num_list[1:])
+        if num_list[0] > max_rest:
+            return num_list[0]
+        else:
+            return max_rest
+        
 
-    
-    
-print(maximum([5,1,7,3,-4,8,11]))
+nums_list = [5, 2, 8, 12, 3]
+maximum = find_max(nums_list)
+print(maximum)
