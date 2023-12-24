24bca0330611c34646f56e5dbfc0c8db465f67a0
myataSD <104759126+myataSD@users.noreply.github.com>
2023-12-24 13:47:22 +0300

    Add files via upload

---
+++ Python libs/Dz2/DataFrameTest.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+data = {'Имя':["Иван", "Мария", "Алексей", "Елена", 'Екатерина', 'Михаил', 'Анна'],
+        'Фамилия':['Иванов', 'Смирнова', 'Петров', 'Сидорова', 'Иванова', 'Кузнецов', 'Петрова'],
+        'Возраст':[25, 32, 41, 19, 28, 41, 24]
+        }
+
+DataF = pd.DataFrame(data)
+print(DataF)
+
+First3 = DataF.head(3)
+print(First3)
+
+Last3 = DataF.tail(3)
+print(Last3)
+
+DataF.to_csv('output.csv')
